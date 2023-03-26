4763d1665e22123eed15cb4e0778d11cc8d1cc6e
KryptoGhost <106913123+KryptoGhost@users.noreply.github.com>
2023-03-26 13:03:52 +0100

    resizing

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const btn = document.querySelector(".explore");
 const display = document.querySelector(".disappear");
+const navBtn = document.querySelector(".navcontrol");
+const nav = document.querySelector(".nav");
 
 btn.addEventListener("click", function() {
    display.classList.toggle("show-disappear");
     //console.log("shake");
+});
+
+navBtn.addEventListener("click", function() {
+    nav.classList.toggle("show-disappear");
 });
